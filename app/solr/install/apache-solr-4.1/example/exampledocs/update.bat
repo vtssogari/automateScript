2b2ca83e23a0bb47458ab7d675d1dda247d27fc0
John Kang <vtssogari@gmail.com>
2013-01-10 00:10:14 -0500

    removing folder

--- app/solr/install/apache-solr-4.1/example/exampledocs/update.bat
+++
@@ -1,2 +0,0 @@
-java -Durl=http://localhost:8080/solr/udid_shard1_replica1/update -jar post.jar *.xml
-pause
--- installation/setenv.bat
+++
@@ -1,2 +0,0 @@
-set JAVA_OPTS=-Xmx5g
-rem set JAVA_OPTS=-Xmx256m -Dhttp.proxyHost=www.xxx.com -Dhttp.proxyPort=80 -Dhttp.nonProxyHosts=*.xxx.com
